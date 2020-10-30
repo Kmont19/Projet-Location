5df41d6ee63c8f90a2541b1210a98d8867e9859f
obf2020 <olivierbfont@gmail.com>
2020-10-30 08:04:56 -0400

    Auto stash before merge of "main" and "origin/main"
    Calendar + notif interface +++

--- RentalProject/AppBundle/UserControls/Notification/INotifCenter.Designer.vb
+++ RentalProject/AppBundle/UserControls/Notification/INotifCenter.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class INotifCenter
     Inherits System.Windows.Forms.UserControl
 
     'UserControl overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -24,7 +24,16 @@ Partial Class INotifCenter
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.SaveButton = New System.Windows.Forms.Button()
+        Me.ResetButton = New System.Windows.Forms.Button()
+        Me.CancelButton = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label8 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
@@ -39,15 +48,105 @@ Partial Class INotifCenter
         '
         'Label1
         '
-        Me.Label1.Location = New System.Drawing.Point(283, 30)
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
         Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label1.Name = "Label2"
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(586, 71)
         Me.Label1.TabIndex = 22
         Me.Label1.Text = "Notification Center"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.SaveButton)
+        Me.Panel2.Controls.Add(Me.ResetButton)
+        Me.Panel2.Controls.Add(Me.CancelButton)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel2.Location = New System.Drawing.Point(0, 382)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(2)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(586, 58)
+        Me.Panel2.TabIndex = 89
+        '
+        'SaveButton
+        '
+        Me.SaveButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(252, Byte), Integer), CType(CType(237, Byte), Integer))
+        Me.SaveButton.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.SaveButton.Dock = System.Windows.Forms.DockStyle.Right
+        Me.SaveButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(163, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.SaveButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(153, Byte), Integer), CType(CType(216, Byte), Integer), CType(CType(153, Byte), Integer))
+        Me.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.SaveButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SaveButton.ForeColor = System.Drawing.Color.FromArgb(CType(CType(58, Byte), Integer), CType(CType(212, Byte), Integer), CType(CType(58, Byte), Integer))
+        Me.SaveButton.Location = New System.Drawing.Point(196, 0)
+        Me.SaveButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.SaveButton.Name = "SaveButton"
+        Me.SaveButton.Size = New System.Drawing.Size(130, 58)
+        Me.SaveButton.TabIndex = 87
+        Me.SaveButton.Text = "Sauvegarder"
+        Me.SaveButton.UseVisualStyleBackColor = False
+        '
+        'ResetButton
+        '
+        Me.ResetButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(237, Byte), Integer), CType(CType(252, Byte), Integer))
+        Me.ResetButton.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ResetButton.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ResetButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(163, Byte), Integer))
+        Me.ResetButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(153, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(216, Byte), Integer))
+        Me.ResetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ResetButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ResetButton.ForeColor = System.Drawing.Color.FromArgb(CType(CType(58, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(212, Byte), Integer))
+        Me.ResetButton.Location = New System.Drawing.Point(326, 0)
+        Me.ResetButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.ResetButton.Name = "ResetButton"
+        Me.ResetButton.Size = New System.Drawing.Size(130, 58)
+        Me.ResetButton.TabIndex = 86
+        Me.ResetButton.Text = "Réinitialiser"
+        Me.ResetButton.UseVisualStyleBackColor = False
+        '
+        'CancelButton
+        '
+        Me.CancelButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(237, Byte), Integer), CType(CType(237, Byte), Integer))
+        Me.CancelButton.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CancelButton.Dock = System.Windows.Forms.DockStyle.Right
+        Me.CancelButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(163, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.CancelButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(216, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(153, Byte), Integer))
+        Me.CancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CancelButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CancelButton.ForeColor = System.Drawing.Color.FromArgb(CType(CType(212, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(58, Byte), Integer))
+        Me.CancelButton.Location = New System.Drawing.Point(456, 0)
+        Me.CancelButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.CancelButton.Name = "CancelButton"
+        Me.CancelButton.Size = New System.Drawing.Size(130, 58)
+        Me.CancelButton.TabIndex = 85
+        Me.CancelButton.Text = "Annuler"
+        Me.CancelButton.UseVisualStyleBackColor = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(196, 100)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(371, 164)
+        Me.RichTextBox1.TabIndex = 90
+        Me.RichTextBox1.Text = ""
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(26, 101)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 91
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(193, 267)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(197, 13)
+        Me.Label8.TabIndex = 97
+        Me.Label8.Text = "Pour modifier un message de notification"
         '
         'INotifCenter
         '
@@ -56,14 +155,28 @@ Partial Class INotifCenter
         Me.AutoSize = True
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(187, Byte), Integer), CType(CType(222, Byte), Integer), CType(CType(251, Byte), Integer))
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.MinimumSize = New System.Drawing.Size(586, 440)
         Me.Name = "INotifCenter"
         Me.Size = New System.Drawing.Size(586, 440)
         Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents SaveButton As Button
+    Friend WithEvents ResetButton As Button
+    Friend WithEvents CancelButton As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label8 As Label
 End Class
